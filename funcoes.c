9b7233631666eb02d69cb4cdaf1e4b15bad1b7d0
Julia Romanetto dos Santos <juliaromanetto@gmail.com>
2024-05-27 19:29:48 -0300

    arq somente com a função menu

---
+++ funcoes.c
@@ -0,0 +1,17 @@
+// arquivo com todas as funções base do programa 
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+
+void exibirMenu(){
+
+    printf("Menu: \n");
+    printf("1 - Gerenciamento de Medalhas\n"
+            "2 - Quiz\n"
+            "3 - Exportar dados\n"
+            "4 - Sair do Programa\n");
+
+    return;
+}
