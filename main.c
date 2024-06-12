6660bf4ba2ddb1737dbae9783f76af90fa067efa
Julia Romanetto dos Santos <juliaromanetto@gmail.com>
2024-06-12 11:20:13 -0300

    menu att

--- main.c
+++ main.c
@@ -14,6 +14,8 @@ int main(){
     // neste caso, caso o arquivo de dados das medalhas seja nulo, signfica que é a primeira vez que o programa está sendo aberto
     if (fp == NULL)
     {
+        
+        printf("Bem vindo ao programa de Medalhas Olímpicas!\n");
 
         // abre o .csv
         fp = fopen("arquivos/medalhas.csv", "r");
@@ -47,13 +49,130 @@ int main(){
         fwrite(medalhas, sizeof(Medalha), contadorLinhas, fp);
         fclose(fp);
 
+        printf("Dados carregados com sucesso!\n");
 
     }else{
-        fclose(fp); // caso o aarq exista, o programa já foi executado antes.
-    }
+        fclose(fp); // caso o arq exista, o programa já foi executado antes.
+    } // if
+
+
 
     // inicia o programa exibindo o menu
-    exibirMenu();
+    int opcao = 0;
+    int opcaoSecundaria = 0;
+
+    printf("Menu: \n");
+    printf("1 - Gerenciamento de Medalhas\n"
+            "2 - Quiz\n"
+            "3 - Exportar dados\n"
+            "4 - Sair do Programa\n");
+
+    
+    // pede uma opção válida
+    do
+    {
+        printf("Deseje a opção desejada: ");
+        scanf("%d", &opcao);
+        if (opcao <= 0 || opcao > 4)
+        {
+            printf("Opção inválida!\n");
+        } // if
+    } while (opcao <= 0 || opcao > 4);
+
+
+    // de acordo com a opção desejada, fornece uma função ou um submenu
+    switch (opcao)
+    {
+    case 1:
+        printf("Você escolheu Gerenciamento de Medalhas!\n");
+        printf("1 - Inserir Medalhas\n"
+            "2 - Alterar Medalhas\n"
+            "3 - Excluir Medalhas\n"
+            "4 - Pesquisar Medalhas\n"
+            "5 - Listar Medalhas\n");
+         do
+        {
+            printf("Deseje a opção desejada: ");
+            scanf("%d", &opcaoSecundaria);
+            if (opcaoSecundaria <= 0 || opcaoSecundaria > 4)
+            {
+                printf("Opção inválida!\n");
+            } // if
+        } while (opcao <= 0 || opcao > 4);
+
+        if (opcaoSecundaria == 1)
+        {
+
+            // realiza o cadastro de medalhas
+            printf("Você Escolheu Inserir Medalhas!");
+            inserirMedalhas(medalhas, &contadorLinhas);
+            printf("Medalhas cadastradas com sucesso! Voltando ao menu.\n\n");
+
+            // exibe o menu novamente
+            printf("Menu: \n");
+            printf("1 - Gerenciamento de Medalhas\n"
+                    "2 - Quiz\n"
+                    "3 - Exportar dados\n"
+                    "4 - Sair do Programa\n");
+
+            
+            // pede uma opção válida
+            do
+            {
+                printf("Deseje a opção desejada: ");
+                scanf("%d", &opcao);
+                if (opcao <= 0 || opcao > 4)
+                {
+                    printf("Opção inválida!\n");
+                } // if
+            } while (opcao <= 0 || opcao > 4);
+
+        }else if (opcaoSecundaria == 2)
+        {
+            printf("Você escolheu a função Alterar Medalhas!\n");
+        }else if (opcaoSecundaria == 3)
+        {
+            printf("Você escolheu a função Excluir Medalhas!\n");
+        }else if (opcaoSecundaria == 4)
+        {
+            printf("Você escolheu a função Pesquisar Medalhas!\n");
+        }else if (opcaoSecundaria == 5)
+        {
+            
+            // exibe a lista de medalhas
+            printf("Você escolheu a função Listar Medalhas!\n\n");
+            listarMedalhas(medalhas, &contadorLinhas);
+            printf("\n\n Todas as medalhas estão listadas acima. Voltando ao menu.\n\n");
+
+            // exibe o menu novamente
+            printf("Menu: \n");
+            printf("1 - Gerenciamento de Medalhas\n"
+                    "2 - Quiz\n"
+                    "3 - Exportar dados\n"
+                    "4 - Sair do Programa\n");
+
+            
+            // pede uma opção válida
+            do
+            {
+                printf("Deseje a opção desejada: ");
+                scanf("%d", &opcao);
+                if (opcao <= 0 || opcao > 4)
+                {
+                    printf("Opção inválida!\n");
+                } // if
+            } while (opcao <= 0 || opcao > 4);
+        } // ultimo if
+
+        break; // case 1
+        
+    case 2:
+        break;
+    default:
+        break;
+    } // switch 
+
+
 
     free(medalhas);
 
